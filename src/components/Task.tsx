55c3e1be78fafa2d36d34d26de9912088bf0fd0b
Clément CARDOT <clementcdt25@gmail.com>
2023-11-20 12:42:14 +0100

    add dropdown for priority

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -68,20 +68,31 @@ const ToDoElement: React.FC<Props> = (props: Props) => {
                             }
                         </button>
 
-                        <div className="spanDiv">
+                        <div className="spanDiv dropdown-center">
                             {
                                 props.task.priority ?
-                                    <span className={"btn badge rounded-pill text-bg-"+props.task.priority} onClick={()=> {}}> 
+                                    <span className={"btn badge rounded-pill dropdown-toggle text-bg-"+props.task.priority} data-bs-toggle="dropdown" aria-expanded="false"> 
                                         {props.task.priority}
                                     </span>
                                 :
-                                    <span className={"btn badge rounded-circle newSpan"} onClick={()=> {}}>
+                                    <span className={"btn badge rounded-circle newSpan"} data-bs-toggle="dropdown" aria-expanded="false">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
                                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                                         </svg>
                                     </span>
                             }
+                            <ul className="dropdown-menu">
+                                <li><span className={"btn badge rounded-pill text-bg-High"}> 
+                                        High
+                                    </span></li>
+                                <li><span className={"btn badge rounded-pill text-bg-Medium"}> 
+                                        Medium
+                                    </span></li>
+                                <li><span className={"btn badge rounded-pill text-bg-Low"}> 
+                                        Low
+                                    </span></li>
+                            </ul>
                         </div>
 
                         <div className="spanDiv">
