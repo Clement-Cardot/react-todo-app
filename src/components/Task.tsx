6963413d30b9cc772cb89b6bd0c74fd60acb6d36
Clément CARDOT <clementcdt25@gmail.com>
2024-01-15 10:41:05 +0100

    last commit

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -49,6 +49,7 @@ const ToDoElement: React.FC<Props> = (props: Props) => {
         props.task.title = value;
         setChangeState(changeState + 1);
         props.autoSave();
+        toogleEditMode();
     }
 
     const handleDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -88,14 +89,13 @@ const ToDoElement: React.FC<Props> = (props: Props) => {
     };
     
     return (
-        <div className="d-flex flex-column" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-            
-        {
-            editMode == 0 ?
-                <div className="d-flex justify-content-between">
-                    <div className="d-flex align-items-center gap-2">
-                        <input className="form-check-input me-3" type="checkbox" defaultChecked={props.task.isDone} onChange={handleCheckChange}/>
+        <div className="d-flex flex-column" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>            
+            <div className="d-flex justify-content-between">
+                <div className="d-flex align-items-center gap-2">
+                    <input className="form-check-input me-3" type="checkbox" defaultChecked={props.task.isDone} onChange={handleCheckChange}/>
 
+                    {
+                    editMode == 0 ?
                         <a className="no-css" href={"/task/" + props.task.id}>
                             <button className="ToDoElementTitle" type="button">
                                 {
@@ -106,39 +106,34 @@ const ToDoElement: React.FC<Props> = (props: Props) => {
                                 }
                             </button>
                         </a>
+                        :
+                        <input className="form-control w-50" type="text" defaultValue={props.task.title} onBlur={handleTitleChange} />
+                    }
 
-                        <KanbanSelector actualStatus={props.task.status} setStatus={setStatus}/>
+                    <KanbanSelector actualStatus={props.task.status} setStatus={setStatus}/>
 
-                        <PrioritySelector actualPriority={props.task.priority} setPriority={setPriority}/>
+                    <PrioritySelector actualPriority={props.task.priority} setPriority={setPriority}/>
 
-                        <TagSelector actualTag={props.task.tag} setTag={setTag}/>
+                    <TagSelector actualTag={props.task.tag} setTag={setTag}/>
 
-                    </div>
-                    
-                    <div className={isHovering? undefined : 'hidden'}>
-                        <button type="button" className="btn" onClick={toogleEditMode}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                            </svg>
-                        </button>
-
-                        <button type="button" className="btn" onClick={removeElement}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                            </svg>
-                        </button>
-                    </div>
                 </div>
-            :
-            <>
-                <div className="form-floating mb-3">
-                    <input type="email" className="form-control" id="floatingInput" defaultValue={props.task.title} onChange={handleTitleChange}/>
-                    <label htmlFor="floatingInput">Task name</label>
+                
+                <div className={isHovering? undefined : 'hidden'}>
+                    <button type="button" className="btn" onClick={toogleEditMode}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                        </svg>
+                    </button>
+
+                    <button type="button" className="btn" onClick={removeElement}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                    </button>
                 </div>
-                <button className="btn btn-primary mb-3" onClick={toogleEditMode}>Submit</button>
-            </>
-        }
+            </div>
+            
         </div>
     );
 };
